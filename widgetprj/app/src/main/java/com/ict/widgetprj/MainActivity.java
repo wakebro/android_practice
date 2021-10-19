6bd2f4270688475f367faec731eb30ebeaf74842
wake <wakebro119@naver.com>
2021-10-19 15:27:08 +0900

    20211019_안드로이드 위젯

--- widgetprj/app/src/main/java/com/ict/widgetprj/MainActivity.java
+++ widgetprj/app/src/main/java/com/ict/widgetprj/MainActivity.java
@@ -2,13 +2,36 @@ package com.ict.widgetprj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv1, tv2, tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = (TextView) findViewById(R.id.textView1);
+        tv2 = (TextView) findViewById(R.id.textView2);
+        tv3 = (TextView) findViewById(R.id.textView3);
+
+        // TextView1의 문장 변경
+        tv1.setText("안녕하세요? TextView 1번입니다.");
+        tv1.setTextColor(Color.CYAN);
+
+        // TextView2의 글씨 크기를 크게 설정
+        // 글꼴은 Serif, 스타일은 italic
+        tv2.setTextSize(50);
+        tv2.setTypeface(Typeface.SERIF, Typeface.ITALIC);
+
+        // TextView3의 문장을 100글자 이상으로 설정
+        // singleLine
+        tv3.setText("가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하");
+        tv3.setSingleLine();
+
     }
 }
