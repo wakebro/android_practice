012cfc2947c4595310b2cdaa468ddd29cb1121bc
wake <wakebro119@naver.com>
2021-10-19 14:24:29 +0900

    20211019_안드로이드 설정

--- HelloAndroid/app/src/main/java/com/ict/HelloAndroid/MainActivity.java
+++ HelloAndroid/app/src/main/java/com/ict/HelloAndroid/MainActivity.java
@@ -10,13 +10,16 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button button1;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 버튼 인식
         button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
 
         // button1 클릭시 이벤트 연결
         button1.setOnClickListener(new View.OnClickListener(){
@@ -24,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // 실제로 일어나는 이벤트는 onClick 메서드 내부에서 진행
                 Toast.makeText(getApplicationContext(), "버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // button2  클릭시 이벤트 연결
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "버튼 클릭!!!", Toast.LENGTH_LONG).show();
             }
         });
     }
