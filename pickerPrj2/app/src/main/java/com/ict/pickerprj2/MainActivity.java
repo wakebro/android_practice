b5be3efa25757eacf817ee4bde51c71f2f8c01d3
wake <wakebro119@naver.com>
2021-10-25 18:15:51 +0900

    20211025_picker

--- pickerPrj2/app/src/main/java/com/ict/pickerprj2/MainActivity.java
+++ pickerPrj2/app/src/main/java/com/ict/pickerprj2/MainActivity.java
@@ -3,12 +3,60 @@ package com.ict.pickerprj2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker dPicker;
+    TimePicker tPicker;
+    Button checkinBtn, checkoutBtn;
+    TextView checkin, checkout;
+    int thisyear, month, day, hour, minute, second;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dPicker = (DatePicker) findViewById(R.id.dPicker);
+        tPicker = (TimePicker) findViewById(R.id.tPicker);
+        checkinBtn = (Button) findViewById(R.id.checkinBtn);
+        checkoutBtn = (Button) findViewById(R.id.checkoutBtn);
+        checkin = (TextView) findViewById(R.id.checkin);
+        checkout = (TextView) findViewById(R.id.checkout);
+
+
+
+        checkinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                thisyear = dPicker.getYear();
+                month = dPicker.getMonth() + 1;
+                day = dPicker.getDayOfMonth();
+                hour = tPicker.getHour() + 9;
+                minute = tPicker.getMinute();
+                checkin.setText(thisyear + "년" + month + "월" + day + "일 " + hour + "시" + minute + "분");
+
+                checkinBtn.setVisibility(View.INVISIBLE);
+                checkoutBtn.setVisibility(View.VISIBLE);
+            }
+        });
+        checkoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                thisyear = dPicker.getYear();
+                month = dPicker.getMonth() + 1;
+                day = dPicker.getDayOfMonth();
+                hour = tPicker.getHour() + 9;
+                minute = tPicker.getMinute();
+                checkout.setText(thisyear + "년" + month + "월" + day + "일 " + hour + "시" + minute + "분");
+
+                checkoutBtn.setVisibility(View.INVISIBLE);
+                checkinBtn.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
